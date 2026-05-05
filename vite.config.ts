259451bfd7ef8ff4491f96f639b506818cc1b7ff
caralovell <168685711+caralovell@users.noreply.github.com>
2026-05-05 11:10:39 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/presentacion-sono/",
   server: {
     host: "::",
     port: 8080,
